22c43b48e62d5a9e0b9e6626e0ed659adb14fd62
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2026-08-29 15:27:14 +0100

    Adding Functions : 1. Find Student 2. Update Student 3. Delete Function

--- main.py
+++ main.py
@@ -44,5 +44,58 @@ def view_students():
         print(f"Phone: {student['phone']}")
         print("_" * 30)
 
-view_students()
-print(view_students)
+# Find Student
+def find_student():
+    student_id = input("Enter Student ID: ")
+
+    found = False
+
+    for student in students:
+        if student["id"] == student_id:
+            print("\n===== Student Found =====")
+            print(f"ID: {student['id']}")
+            print(f"Name: {student['name']}")
+            print(f"Age: {student['age']}")
+            print(f"Department: {student['department']}")
+            print(f"Level: {student['level']}")
+            print(f"Email: {student['email']}")
+            print(f"Phone: {student['phone']}")
+
+            found = True
+            break
+    if not found:
+         print(f"Student with ID {student_id} not found.")
+
+# Update Student
+def update_student():
+       student_id = input("Enter Student ID: ")
+
+       for student in students:
+           if student["id"] == student_id:
+               print("\n===== Update Student =====")
+               print(f"ID: {student['id']}")
+               print(f"Name: {student['name']}")
+               print(f"Age: {student['age']}")
+
+               new_name = input("Enter new name: ")
+               new_age = int(input("Enter new age: "))
+
+               student["name"] = new_name
+               student["age"] = new_age
+
+               print("Student Updated Successfully!.")
+               return
+       print(f"Student with ID {student_id} not found.")
+
+# Delete
+def delete_student():
+    student_id = input("Enter Student ID: ")
+
+    for student in students:
+        if student["id"] == student_id:
+         students.remove(student)
+         print("Student deleted successfully!.")
+         return
+
+    print(f"Student with ID {student_id} not found.")
+
